f535f9828cc557db58af620b331b513d284fc08f
benelan <benelan@yahoo.com>
2019-10-23 13:18:34 -0700

    bug fix

--- server.js
+++ server.js
@@ -8,22 +8,24 @@ const app = require("express")(),
 app.use(helmet());
 app.use(helmet.referrerPolicy({ policy: 'no-referrer' }))
 
+io.on('connection', (socket) => {
+    request.post({
+        url: 'https://www.arcgis.com/sharing/rest/generateToken',
+        json: true,
+        form: {
+            'f': 'json',
+            'username': 'USERNAME', // change this
+            'password': 'PASSWORD', // this too
+            'referer': 'localhost'  // and this for prod
+        }
+    }, function (error, response, body) {
+        io.emit("msg", { body });
+    });
+});
+
 app.get('/', (req, res) => {
     res.sendFile('map.html', { root: path.join(__dirname, '/') });
-    io.on('connection', (socket) => {
-        request.post({
-            url: 'https://www.arcgis.com/sharing/rest/generateToken',
-            json: true,
-            form: {
-                'f': 'json',
-                'username': 'USERNAME', // change this
-                'password': 'PASSWORD', // this too
-                'referer': 'localhost'  // and this for prod
-            }
-        }, function (error, response, body) {
-            io.emit("msg", { body });
-        });
-    });
+    
 })
 
 server.listen(process.env.PORT || 8001);
